7c90c686e09274249cd1c8386e3ba82cd27cb5e7
Brandon Swett <bran.swett@gmail.com>
2020-11-01 12:35:55 -0500

    Added a basic background video to play during the initial screen when the user chooses whether they want to log in or register

--- Login-Attempt-2/HomeViewController.swift
+++ Login-Attempt-2/HomeViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
-
+    var player: AVPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        playBackgroundVideo()
         // Do any additional setup after loading the view.
+    }
+    
+    func playBackgroundVideo(){
+        let path = Bundle.main.path(forResource: "IMG_0395", ofType: ".mp4")
+        player = AVPlayer(url: URL(fileURLWithPath: path!))
+        player!.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.view.layer.insertSublayer(playerLayer, at: 0)
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player!.currentItem)
+        player!.seek(to: CMTime.zero)
+        player!.play()
+        self.player?.isMuted = true
+    }
+    
+    @objc func playerItemDidReachEnd(){
+        player!.seek(to: CMTime.zero)
     }
     
 
